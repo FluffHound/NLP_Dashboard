85f607094d9073a56ec1d71883117041f4a7cbbe
Muhammad Hanif Sudibyo <mhsudibyo@gmail.com>
2022-11-24 20:28:20 +0700

    Scrape data all-time 24/11/22
    Scrape data + preprocess data 24/11/22 serta penambahan pipeline scrap

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # NLP_Dashboard
-Yeey proyek
+*\#UNARIRHEBAT*
+Proyek dibuat untuk memenuhi tugas mata kuliah Natural Language Processing semester 5
+
+This project is made to fulfill Natural Language Processing course final project task
+
+Anggota/Team Members - NIM/Student ID:
+- Sabrina Tiara Bachtiar	(162012133004)
+- Najma Attaqiya Alya		(162012133015)
+- Ilham Ahmad Kamil			(162012133019)
+- Muhammad Alfian Pratama	(162012133037)
+- Muhammad Hanif Sudibyo	(162012133040)
+
+Data discrap dari Twitter, menggunakan package Python SNScrape. Topik yang dicari adalah tweet dari profil masing-masing kandidat calon presiden yang masuk pada survey elektabilitas 2024, serta tweet masyarakat pada tag masing-masing calon kandidat.
+
+Data scraped from Twitter, using SNScrape Python package. Topics searched is Indonesian's eligible candidate for Presidential Election 2024 user profile tweets and other's mention of each candidate's tag.
---
+++ Scraper/Scrap Pipeline.py
@@ -0,0 +1,102 @@
+# ===========================
+# ===== Import Packages =====
+# ===========================
+import pandas as pd
+import numpy as np
+from tqdm import tqdm
+
+import snscrape.modules.twitter as sntwitter
+
+import re, string
+from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
+from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
+
+from wordcloud import WordCloud
+import matplotlib.pyplot as plt
+
+# ===============================
+# ===== Preprocess Function =====
+# ===============================
+# ___Remove Rubbish___
+def clean_text(text):
+    text = str(text)
+    text = text.lower()
+    text = re.sub("@[A-Za-z0-9_]+", "", text)  # Menghapus @<name> [mention twitter]
+    text = re.sub("#\w+", "", text) # Menghapus hashtag #
+    text = re.sub("\[.*?\]", "", text)  # Menghapus [string]
+    text = re.sub("https?://\S+|www\.\S+", "", text)    # Menghapus link
+    text = re.sub("<.*?>+", "", text)   # Menghapus <string>
+    text = re.sub("[%s]" % re.escape(string.punctuation), "", text) # Menghapus tanda baca
+    text = re.sub("\w*\d\w*", "", text) # Menghapus kata + angka ex: t0g3l
+    text = re.sub("\d+", "", text)  # Menghapus kata diawali huruf ex: 45merdeka
+    text = re.sub("amp", " ", text) # Menghapus &amp; pada kata
+    text = re.sub("\s+", " ", text).strip() # Menghapus whitespace di antara kata
+    text = text.replace("\n", " ")  # Menghapus tag newline
+    text = " ".join(text.split())
+    return text
+
+# ___Stemming___
+factory = StemmerFactory()
+stemmer = factory.create_stemmer()
+
+# ___Stopword___
+factory_stop = StopWordRemoverFactory()
+stopword = factory_stop.create_stop_word_remover()
+
+# ===== Things to Query =====
+usernames = ['ganjarpranowo', 'prabowo', 'aniesbaswedan', 'AgusYudhoyono', 'ridwankamil']
+hashtags = ['ganjarpranowofor2024', 'prabowo', 'AniesPresiden2024', 'AHY', 'RidwanKamil']
+
+# ==========================
+# ===== Scrap Hashtags =====
+# ==========================
+for user in range(len(hashtags)):
+    print('\n' + 'Scraping #' + hashtags[user] + '...')
+    # ___Hashtag scraper___
+    tweets_list = []
+
+    for i, tweet in enumerate(tqdm(sntwitter.TwitterHashtagScraper(hashtags[user]).get_items())):
+        if i > 1000:
+            break
+        tweets_list.append([tweet.id, tweet.date, tweet.user.username, tweet.content])
+
+    tweets_df = pd.DataFrame(tweets_list, columns=['ID', 'Datetime', 'Username', 'Content'])
+    raw_tweets_df = tweets_df.copy()
+
+    # ___Stemming & Remove Stopwords___
+    tweets_df['clean_text'] = tweets_df['Content'].apply(lambda x: clean_text(x))
+    tweets_df['clean_text'] = tweets_df['clean_text'].apply(lambda x: stopword.remove(x))
+
+    # ___Export Data___
+    raw_tweets_df.to_csv('./Data Raw/raw_hashtag_{}.csv'.format(hashtags[user]), sep=';', index=False)
+    tweets_df.to_csv('./Data Clean/clean_hashtag_{}.csv'.format(hashtags[user]), sep=';', index=False)
+
+print('\n' + '========================= SCRAPE HASHTAG DONE =========================' + '\n')
+
+# =======================================
+# ===== Scrap User Profile's Tweets =====
+# =======================================
+for user in range(len(usernames)):
+    print('\n' + 'Scraping @' + usernames[user] + '...')
+    # ___User Profile scraper___
+    user_profile_list = []
+
+    for i, tweet in enumerate(tqdm(sntwitter.TwitterUserScraper(usernames[user]).get_items())):
+        if i > 1000:
+            break
+        user_profile_list.append([tweet.id, tweet.date, tweet.likeCount, tweet.content])
+
+    user_tweets_df = pd.DataFrame(user_profile_list, columns=['ID', 'Datetime', 'Likes', 'Content'])
+    raw_user_tweets_df = user_tweets_df.copy()
+
+    # ___Stemming & Remove Stopwords___
+    user_tweets_df['clean_text'] = user_tweets_df['Content'].apply(lambda x: clean_text(x))
+    user_tweets_df['clean_text'] = user_tweets_df['clean_text'].apply(lambda x: stopword.remove(x))
+
+    # ___Export Data___
+    raw_user_tweets_df.to_csv('./Data Raw/raw_userProfile_{}.csv'.format(usernames[user]), sep=';', index=False)
+    user_tweets_df.to_csv('./Data Clean/clean_userProfile_{}.csv'.format(usernames[user]), sep=';', index=False)
+
+print('\n' + '========================= SCRAPE USER PROFILE DONE =========================' + '\n')
+
+print('ALL DONE!')
