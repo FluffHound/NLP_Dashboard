f644cd2b2c7b370965884f23cd31052c0416f926
Muhammad Hanif Sudibyo <mhsudibyo@gmail.com>
2022-11-25 21:49:14 +0700

    Fix scraper, add base topic modeling

--- Scraper/scrap_pipeline.py
+++ Scraper/scrap_pipeline.py
@@ -10,6 +10,7 @@ import snscrape.modules.twitter as sntwitter
 import re, string
 from Sastrawi.Stemmer.StemmerFactory import StemmerFactory
 from Sastrawi.StopWordRemover.StopWordRemoverFactory import StopWordRemoverFactory
+from nltk.tokenize import word_tokenize
 
 from wordcloud import WordCloud
 import matplotlib.pyplot as plt
@@ -43,7 +44,13 @@ stemmer = factory.create_stemmer()
 factory_stop = StopWordRemoverFactory()
 stopword = factory_stop.create_stop_word_remover()
 
+# ___Tokenizing___
+def word_tokenize_wrapper(text):
+  return word_tokenize(text)
+
+# ===========================
 # ===== Things to Query =====
+# ===========================
 hashtags = ['ganjarpranowofor2024', 'prabowo', 'AniesPresiden2024', 'AHY', 'RidwanKamil']
 usernames = ['ganjarpranowo', 'prabowo', 'aniesbaswedan', 'AgusYudhoyono', 'ridwankamil']
 keywords = ['@ganjarpranowo', '@prabowo', '@aniesbaswedan', '@AgusYudhoyono', '@ridwankamil']
@@ -57,18 +64,22 @@ for user in range(len(hashtags)):
     tweets_list = []
 
     for i, tweet in enumerate(tqdm(sntwitter.TwitterHashtagScraper(hashtags[user]).get_items())):
-        if i > 1000:
+        if i > 50:
             break
         tweets_list.append([tweet.id, tweet.date, tweet.user.username, tweet.content])
 
     tweets_df = pd.DataFrame(tweets_list, columns=['ID', 'Datetime', 'Username', 'Content'])
 
-    # ___Stemming & Remove Stopwords___
+    # ___Cleaning, Stemming, Remove Stopwords, Remove Blank Text, Tokenize___
     tweets_df['clean_text'] = tweets_df['Content'].apply(lambda x: clean_text(x))
+    tweets_df['clean_text'] = tweets_df['clean_text'].apply(lambda x: stemmer.stem(x))
     tweets_df['clean_text'] = tweets_df['clean_text'].apply(lambda x: stopword.remove(x))
+    tweets_df.replace("", float("NaN"), inplace=True)
+    tweets_df = tweets_df.dropna()
+    tweets_df['text_token'] = tweets_df['clean_text'].apply(word_tokenize_wrapper)
 
     # ___Export Data___
-    tweets_df.to_csv('./Data Clean/hashtag_{}.csv'.format(hashtags[user]), sep=';', index=False)
+    tweets_df.to_csv('./data_clean/hashtag_{}.csv'.format(hashtags[user]), sep=';', index=False)
 
 print('\n' + '========================= SCRAPE HASHTAG DONE =========================' + '\n')
 
@@ -81,18 +92,22 @@ for user in range(len(usernames)):
     user_profile_list = []
 
     for i, tweet in enumerate(tqdm(sntwitter.TwitterUserScraper(usernames[user]).get_items())):
-        if i > 1000:
+        if i > 50:
             break
         user_profile_list.append([tweet.id, tweet.date, tweet.likeCount, tweet.content])
 
     user_tweets_df = pd.DataFrame(user_profile_list, columns=['ID', 'Datetime', 'Likes', 'Content'])
 
-    # ___Stemming & Remove Stopwords___
+# ___Cleaning, Stemming, Remove Stopwords, Remove Blank Text, Tokenize___
     user_tweets_df['clean_text'] = user_tweets_df['Content'].apply(lambda x: clean_text(x))
+    user_tweets_df['clean_text'] = user_tweets_df['clean_text'].apply(lambda x: stemmer.stem(x))
     user_tweets_df['clean_text'] = user_tweets_df['clean_text'].apply(lambda x: stopword.remove(x))
+    user_tweets_df.replace("", float("NaN"), inplace=True)
+    user_tweets_df = user_tweets_df.dropna()
+    user_tweets_df['text_token'] = user_tweets_df['clean_text'].apply(word_tokenize_wrapper)
 
     # ___Export Data___
-    user_tweets_df.to_csv('./Data Clean/userProfile_{}.csv'.format(usernames[user]), sep=';', index=False)
+    user_tweets_df.to_csv('./data_clean/userProfile_{}.csv'.format(usernames[user]), sep=';', index=False)
 
 print('\n' + '========================= SCRAPE USER PROFILE DONE =========================' + '\n')
 
@@ -105,18 +120,22 @@ for user in range(len(keywords)):
     user_mention_list = []
 
     for i, tweet in enumerate(tqdm(sntwitter.TwitterSearchScraper(keywords[user]).get_items())):
-        if i > 1000:
+        if i > 50:
             break
         user_mention_list.append([tweet.id, tweet.date, tweet.user.username, tweet.content])
 
     mentions_tweets_df = pd.DataFrame(user_mention_list, columns=['ID', 'Datetime', 'Username', 'Content'])
 
-    # ___Stemming & Remove Stopwords___
+    # ___Cleaning, Stemming, Remove Stopwords, Remove Blank Text, Tokenize___
     mentions_tweets_df['clean_text'] = mentions_tweets_df['Content'].apply(lambda x: clean_text(x))
+    mentions_tweets_df['clean_text'] = mentions_tweets_df['clean_text'].apply(lambda x: stemmer.stem(x))
     mentions_tweets_df['clean_text'] = mentions_tweets_df['clean_text'].apply(lambda x: stopword.remove(x))
+    mentions_tweets_df.replace("", float("NaN"), inplace=True)
+    mentions_tweets_df = mentions_tweets_df.dropna()
+    mentions_tweets_df['text_token'] = mentions_tweets_df['clean_text'].apply(word_tokenize_wrapper)
 
     # ___Export Data___
-    mentions_tweets_df.to_csv('./Data Clean/userMention_{}.csv'.format(keywords[user]), sep=';', index=False)
+    mentions_tweets_df.to_csv('./data_clean/userMention_{}.csv'.format(keywords[user]), sep=';', index=False)
 
 print('\n' + '========================= SCRAPE USER MENTION DONE =========================' + '\n')
 
